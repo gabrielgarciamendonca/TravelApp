af9349ac20afaef467281a4b6fc66b8b83b3f55a
Gabriel Mendonca <gpm@lionx.com.br>
2022-06-08 22:34:23 -0300

    Inserted first image, removed TitleBar just from MainActivity.kt and put translucent statusBar on themes.xml

--- app/src/main/java/com/pandora/travelapp/MainActivity.kt
+++ app/src/main/java/com/pandora/travelapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.pandora.travelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.TextView
+import com.pandora.travelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }

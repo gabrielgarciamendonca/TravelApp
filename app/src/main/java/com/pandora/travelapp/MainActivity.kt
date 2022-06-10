8ef63d864371737ce74d3a2de8d4a6c62a829028
Gabriel Mendonca <gpm@lionx.com.br>
2022-06-09 23:38:15 -0300

    Inserted icon in app, modified themes.xml to new colors palette and included more components into main screen

--- app/src/main/java/com/pandora/travelapp/MainActivity.kt
+++ app/src/main/java/com/pandora/travelapp/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.pandora.travelapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.view.Window
-import android.widget.TextView
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.pandora.travelapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,5 +16,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w = window
+            w.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
     }
 }
